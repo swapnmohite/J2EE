cb997bd82935cad2fd6b970ecf6fcfcda8c9a6c7
Swapnil Mohite <swapnilmohite495@gmail.com>
2023-08-25 12:22:52 +0530

    Added JSP project

--- multithreading/src/com/jspiders/multithreading/thread/Husband.java
+++ multithreading/src/com/jspiders/multithreading/thread/Husband.java
@@ -3,17 +3,16 @@ package com.jspiders.multithreading.thread;
 import com.jspiders.multithreading.resource.Account;
 
 public class Husband extends Thread {
-	
+
 	private Account account;
-	
+
 	public Husband(Account account) {
 		this.account = account;
 	}
-	
+
 	@Override
 	public void run() {
 		account.deposit(5000);
 		account.withdraw(2000);
 	}
-
 }
--- multithreading/src/com/jspiders/multithreading/thread/Wife.java
+++ multithreading/src/com/jspiders/multithreading/thread/Wife.java
@@ -3,17 +3,16 @@ package com.jspiders.multithreading.thread;
 import com.jspiders.multithreading.resource.Account;
 
 public class Wife extends Thread {
-	
+
 	private Account account;
-	
+
 	public Wife(Account account) {
 		this.account = account;
 	}
-	
+
 	@Override
 	public void run() {
 		account.deposit(2000);
 		account.withdraw(5000);
 	}
-
 }
