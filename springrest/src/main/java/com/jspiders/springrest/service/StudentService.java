8886d7ad1bc730014a03b39d32c01063e9387925
Swapnil Mohite <swapnilmohite495@gmail.com>
2023-09-13 13:38:50 +0530

    SEARCH ALL, REMOVE and UPDATE features - REST

--- springrest/src/main/java/com/jspiders/springrest/service/StudentService.java
+++ springrest/src/main/java/com/jspiders/springrest/service/StudentService.java
@@ -1,5 +1,7 @@
 package com.jspiders.springrest.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import com.jspiders.springrest.repository.StudentRepository;
 
 @Service
 public class StudentService {
+
 	@Autowired
 	private StudentRepository repository;
 
@@ -16,4 +19,23 @@ public class StudentService {
 		return student;
 	}
 
+	public StudentPOJO searchStudent(int id) {
+		StudentPOJO student = repository.searchStudent(id);
+		return student;
+	}
+
+	public List<StudentPOJO> searchAllStudent() {
+		List<StudentPOJO> students = repository.searchAllStudents();
+		return students;
+	}
+
+	public StudentPOJO removeStudent(int id) {
+		StudentPOJO student = repository.removeStudent(id);
+		return student;
+	}
+
+	public StudentPOJO updateStudent(StudentPOJO pojo) {
+		StudentPOJO student = repository.updateStudent(pojo);
+		return student;
+	}
 }
