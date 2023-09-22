52bc2b07f2e87ce0121b924fcaa87313bd2eab9e
Swapnil Mohite <swapnilmohite495@gmail.com>
2023-09-22 21:55:30 +0530

    fix errors

--- springrest/src/main/java/com/jspiders/springrest/repository/AdminRepository.java
+++ springrest/src/main/java/com/jspiders/springrest/repository/AdminRepository.java
@@ -4,7 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+//import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
@@ -16,8 +16,8 @@ public class AdminRepository {
 	private static EntityManagerFactory factory;
 	private static EntityManager manager;
 	private static EntityTransaction transaction;
-	private static Query query;
-	private static String jpql;
+//	private static Query query;
+//	private static String jpql;
 
 	private static void openConnection() {
 		factory = Persistence.createEntityManagerFactory("REST");
